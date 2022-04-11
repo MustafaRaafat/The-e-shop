8d67cde9d6d19e27eee5028f4d9e1f69f58d9de6
Mahmoud Easa <mu01011422865@gmail.com>
2022-04-11 16:25:44 +0200

    Admin v1.1

--- website/admin/js/nav.js
+++ website/admin/js/nav.js
@@ -11,21 +11,23 @@ const searchNav = document.querySelector(".nav-top .left .form input");
 const searchDiv = document.querySelector(".nav-top .left .form .search-div");
 // Side Bar Link Pages
 const linkPages = document.querySelectorAll(".nav-left .nav-title");
-// Side Bar Minue Pages
+// Side Bar Menu Pages
 const menuPages = document.querySelectorAll(".nav-left .nav-title ul");
+// Side Bar Menu Container
+const menuContainer = document.querySelector(".simplebar-content-wrapper");
 // Meida Quaries
 const max992 = window.matchMedia("(max-width: 992px)");
-const min993 = window.matchMedia("(min-width: 993px)");
 
-
-//   if(max992.matches) {
-//     sideBar.classList.add("hide");
-//     mainRight.classList.add("main-width");
-//     sideBar.style.zIndex = "99999999";
-//   }else if(min993.matches) {
-//     sideBar.classList.remove("hide");
-//     mainRight.classList.remove("main-width");
-//   }
+// Hide Sidebar When Window Resize
+window.addEventListener("resize", () => {
+    if(max992.matches) {
+      if(sideBar.className.includes("hide")
+        || mainRight.className.includes("main-width")) {
+          sideBar.classList.remove("hide");
+          mainRight.classList.remove("main-width");
+        }
+    }
+  });
 
 // Side Bar Open Minue Pages
 function openMenu() {
@@ -50,18 +52,8 @@ function toggleSideBar() {
 }
 toggleSideBar();
 
-// document.onresize(() => {
-//   if(max992.matches) {
-//     sideBar.classList.add("hide");
-//     mainRight.classList.add("main-width");
-//   }else if(min993.matches) {
-//     sideBar.classList.remove("hide");
-//     mainRight.classList.remove("main-width");
-//   }
-// })
-
-// Toggle Sidebar When Press Escape
-function escapeToToggleSideBar() {
+// Hide Sidebar When Press Escape
+function escapeToHideSideBar() {
   document.addEventListener("keydown", (e) => {
     if(max992.matches && e.key === "Escape"
         && sideBar.className.includes("hide")) {
@@ -69,13 +61,41 @@ function escapeToToggleSideBar() {
       }
     });
 }
-escapeToToggleSideBar();
+escapeToHideSideBar();
+
+// Hide Sidebar When Click On Document
+function clickToHideSideBar() {
+  document.addEventListener("click", (e) => {
+    if(max992.matches
+      && e.target !== menuContainer
+      && e.target !== toggleButton) {
+        
+      sideBar.classList.remove("hide");
+    }
+    });
+    // Stop Propagation On Menu Container
+    menuContainer.onclick = function (e) {
+    e.stopPropagation();
+  }
+  }
+  clickToHideSideBar();
+  
+
 
 // Search Nav
   searchNav.addEventListener("focus", () => {
     searchDiv.style.display = "flex";
   });
-  searchNav.addEventListener("blur", () => {
-    searchDiv.style.display = "none";
-  });
 
+  document.addEventListener("click", (e) => {
+    if(e.target !== searchNav
+      && e.target !== searchDiv) {
+        
+        searchDiv.style.display = "none";
+    }
+    });
+
+  // Stop Propagation On Search Div
+  searchDiv.onclick = function (e) {
+    e.stopPropagation();
+  }
