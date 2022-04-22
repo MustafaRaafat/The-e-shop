1dae67c10d8872778f6eb911ca287b4f20f8afb9
Mustafa <mustafa.raft@hotmail.com>
2022-04-22 22:06:08 +0200

    v 1.0

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(cors());
 app.use(express.static('website'));
 
 const port = 8000;
-const server = app.listen(port, listening);
+const server = app.listen(process.env.PORT||port, listening);
 function listening() {
     console.log("server running");
     console.log(`running on localhost: ${port}`);
